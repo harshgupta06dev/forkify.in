beae23912c429d5289a06b397868f10059e30acf
HarshGupta <harshgupta06dev@gmail.com>
2025-11-26 07:31:49 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '478216af-fac1-402a-90a8-489672853a6d';
+export const MODAL_CLOSE_SEC = 2.5;
